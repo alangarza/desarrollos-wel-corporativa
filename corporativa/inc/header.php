9d0b041c731da7f2aa3287eaa7fdd51cba14385e
Alan <alan@Alan-MacBook-Pro.local>
2018-11-09 11:38:14 -0600

    index & header removed .php in hrefs

--- corporativa/inc/header.php
+++ corporativa/inc/header.php
@@ -54,7 +54,7 @@
         <div class="row">
             <div class="col-12">
                 <nav class="navbar navbar-expand-lg navbar-light rounded">
-                    <a class="navbar-brand logo navbar-brand d-flex w-50 mr-auto" href="index.php">
+                    <a class="navbar-brand logo navbar-brand d-flex w-50 mr-auto" href="index">
                         <img src="assets/img/logos/logo-dark.png" alt="logo">
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
